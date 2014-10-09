7c5acbe685164a1766504c0b73b20a20c02d8479
FayeMD <fayemcdonald@hotmail.com>
2014-10-09 11:50:40 -0300

    Help from Matt

---
+++ index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+	res.sendfile('index.html');
+});
+app.use("/style.css", express.static(__dirname + '/style.css'));
+app.use("/script.js", express.static(__dirname + '/script.js'));
+
+io.on('connection', function(socket){
+	socket.on('chat message', function(msg){
+		io.emit('chat message', msg);
+	});
+});
+
+http.listen(80, function(){
+	console.log('listening on *:80');
+});
