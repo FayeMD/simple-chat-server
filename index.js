7495c2d0c722358ae301ded5c474f38762f5f048
FayeMD <fayemcdonald@hotmail.com>
2014-10-15 15:40:55 -0300

    5

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
-console.log("script loaded");
-
 var express = require('express'),
 	app = express(),
-	http = require('http').Server(app);
+	server = require('http').createServer(app),
+	io = require('socket.io').listen(server);
 
-	http.listen(3000, function(){
+server.listen(3000, function(){
 	console.log('listening on *:3000');
 });
-	var io = require('socket.io')(http);
+	
 
 
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html');
+});
+
+io.sockets.on('connection', function(socket){
+		socket.on('send message', function(data){
+			io.sockets.emit('new message', data);
+	});
 });
 
 
@@ -23,13 +28,6 @@ app.use("/heart.jpg", express.static(__dirname + '/heart.jpg'));
 app.use("/sky.jpg", express.static(__dirname + '/sky.jpg'));
 app.use("/lemonade.jpg", express.static(__dirname + '/lemonade.jpg'));
 
-io.on('connection', function(socket){
-	console.log("connection");
-	socket.on('chat message', function(msg){
-		io.emit('chat message', msg);
-		console.log('message: ' + msg);
-	});
-});
 
 
 
